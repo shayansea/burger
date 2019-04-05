b8c191b544dad29d5c40f6a71390ea73cf54b4e9
Shayan Anoushiravani <shayananoushiravani@Shayans-MacBook-Pro.local>
2019-04-05 16:55:58 -0700

    added ded file structure

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Classic Quarter Pounder', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Double', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('French Fries', true);
